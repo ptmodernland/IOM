fa562aabe970f4b627d6863ae6f8cd90e2a3cf55
titikkoma88 <jie.koma88@gmail.com>
2019-09-19 13:50:27 +0700

    Menambahkan Menu Barang Jasa

--- app/src/main/java/iom/modernland/co/id/MyFirebaseMessagingService.java
+++ app/src/main/java/iom/modernland/co/id/MyFirebaseMessagingService.java
@@ -48,7 +48,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, HomeUserActivity.class);
+        Intent intent = new Intent(this, ContentApproveActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
