e15ce5b00a2949c40ee4d783ef01b80700013d8c
titikkoma88 <jie.koma88@gmail.com>
2020-01-20 17:34:05 +0700

    Membuat Kordinasi PBJ

---
+++ app/src/main/java/iom/modernland/co/id/ContentKordinasiPBJActivity.java
@@ -0,0 +1,13 @@
+package iom.modernland.co.id;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContentKordinasiPBJActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_kordinasi_pbj);
+    }
+}
---
+++ app/src/main/java/iom/modernland/co/id/KordinasiPBJAdapter.java
@@ -0,0 +1,4 @@
+package iom.modernland.co.id;
+
+public class KordinasiPBJAdapter {
+}
